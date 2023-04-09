b6c45318a3d9bf6867f9e62c7902f4ad162fffde
Aaron Colwell <300262+acolwell@users.noreply.github.com>
2023-04-08 18:42:05 -0700

    Fix opencolorio on Windows.

--- recipes/opencolorio/all/conanfile.py
+++ recipes/opencolorio/all/conanfile.py
@@ -1,5 +1,8 @@
+from conan import ConanFile, Version
+from conan.tools.build import check_min_cppstd
+from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
+from conan.tools.files import apply_conandata_patches, export_conandata_patches, copy, get, replace_in_file, rm, rmdir
 from conan.tools.microsoft import is_msvc
-from conans import ConanFile, CMake, tools
 import functools
 import os
 
@@ -26,97 +29,92 @@ class OpenColorIOConan(ConanFile):
         "use_sse": True,
     }
 
-    generators = "cmake", "cmake_find_package"
-
-    @property
-    def _source_subfolder(self):
-        return "source_subfolder"
-
-    @property
-    def _build_subfolder(self):
-        return "build_subfolder"
-
     def export_sources(self):
-        self.copy("CMakeLists.txt")
-        for patch in self.conan_data.get("patches", {}).get(self.version, []):
-            self.copy(patch["patch_file"])
+        export_conandata_patches(self)
 
     def config_options(self):
         if self.settings.os == "Windows":
-            del self.options.fPIC
+            self.options.rm_safe("fPIC")
         if self.settings.arch not in ["x86", "x86_64"]:
-            del self.options.use_sse
+            self.options.rm_safe("use_sse")
 
     def configure(self):
         if self.options.shared:
-            del self.options.fPIC
+            self.options.rm_safe("fPIC")
 
     def requirements(self):
         self.requires("expat/2.4.8")
-        self.requires("openexr/2.5.7")
         self.requires("yaml-cpp/0.7.0")
-        if tools.Version(self.version) < "2.0.0":
+        if Version(self.version) < "2.0.0":
+            self.requires("openexr/2.5.7")
             self.requires("tinyxml/2.6.2")
         else:
+            self.requires("imath/3.1.6")
+            self.requires("openexr/3.1.5")
             self.requires("pystring/1.1.3")
         # for tools only
-        self.requires("lcms/2.13.1")
+        self.requires("lcms/2.14")
         # TODO: add GLUT (needed for ociodisplay tool)
 
     def validate(self):
         if self.settings.compiler.get_safe("cppstd"):
-            tools.check_min_cppstd(self, 11)
+            check_min_cppstd(self, 11)
+
+    def layout(self):
+        cmake_layout(self, src_folder="src")
 
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version],
-                  destination=self._source_subfolder, strip_root=True)
+        get(self, **self.conan_data["sources"][self.version],
+            destination=self.source_folder, strip_root=True)
+
+        apply_conandata_patches(self)
+
+        for module in ("expat", "lcms2", "pystring", "yaml-cpp", "Imath"):
+            rm(self, pattern="Find"+module+".cmake", folder=os.path.join(self.source_folder, "share", "cmake", "modules"), recursive=True)
+
+    def generate(self):
+        deps = CMakeDeps(self)
+        deps.generate()
+        tc = CMakeToolchain(self)
+
+        if Version(self.version) >= "2.1.0":
+            tc.cache_variables["OCIO_BUILD_PYTHON"] = False
+        else:
+            tc.cache_variables["OCIO_BUILD_SHARED"] = self.options.shared
+            tc.cache_variables["OCIO_BUILD_STATIC"] = not self.options.shared
+            tc.cache_variables["OCIO_BUILD_PYGLUE"] = False
+            tc.cache_variables["USE_EXTERNAL_YAML"] = True
+            tc.cache_variables["USE_EXTERNAL_TINYXML"] = True
+            tc.cache_variables["USE_EXTERNAL_LCMS"] = True
+
+        tc.cache_variables["OCIO_USE_SSE"] = self.options.get_safe("use_sse", False)
+
+        if self.dependencies["openexr"].ref.version < "3.0.0":   
+            # openexr 2.x provides Half library
+            tc.cache_variables["OCIO_USE_OPENEXR_HALF"] = True
+        else:
+            tc.cache_variables["OCIO_USE_OPENEXR_HALF"] = False
+
+        tc.cache_variables["OCIO_BUILD_APPS"] = True
+        tc.cache_variables["OCIO_BUILD_DOCS"] = False
+        tc.cache_variables["OCIO_BUILD_TESTS"] = False
+        tc.cache_variables["OCIO_BUILD_GPU_TESTS"] = False
+
+        # avoid downloading dependencies
+        tc.cache_variables["OCIO_INSTALL_EXT_PACKAGE"] = "NONE"
+
+        if is_msvc(self) and not self.options.shared:
+            # define any value because ifndef is used
+            tc.cache_variables["OpenColorIO_SKIP_IMPORTS"] = True
+        tc.generate()
 
     @functools.lru_cache(1)
     def _configure_cmake(self):
         cmake = CMake(self)
-
-        if tools.Version(self.version) >= "2.1.0":
-            cmake.definitions["OCIO_BUILD_PYTHON"] = False
-        else:
-            cmake.definitions["OCIO_BUILD_SHARED"] = self.options.shared
-            cmake.definitions["OCIO_BUILD_STATIC"] = not self.options.shared
-            cmake.definitions["OCIO_BUILD_PYGLUE"] = False
-
-            cmake.definitions["USE_EXTERNAL_YAML"] = True
-            cmake.definitions["USE_EXTERNAL_TINYXML"] = True
-            cmake.definitions["USE_EXTERNAL_LCMS"] = True
-
-        cmake.definitions["OCIO_USE_SSE"] = self.options.get_safe("use_sse", False)
-
-        # openexr 2.x provides Half library
-        cmake.definitions["OCIO_USE_OPENEXR_HALF"] = True
-
-        cmake.definitions["OCIO_BUILD_APPS"] = True
-        cmake.definitions["OCIO_BUILD_DOCS"] = False
-        cmake.definitions["OCIO_BUILD_TESTS"] = False
-        cmake.definitions["OCIO_BUILD_GPU_TESTS"] = False
-        cmake.definitions["OCIO_USE_BOOST_PTR"] = False
-
-        # avoid downloading dependencies
-        cmake.definitions["OCIO_INSTALL_EXT_PACKAGE"] = "NONE"
-
-        if is_msvc(self) and not self.options.shared:
-            # define any value because ifndef is used
-            cmake.definitions["OpenColorIO_SKIP_IMPORTS"] = True
-
-        cmake.configure(build_folder=self._build_subfolder)
+        cmake.configure()
         return cmake
 
-    def _patch_sources(self):
-        for patch in self.conan_data.get("patches", {}).get(self.version, []):
-            tools.patch(**patch)
-
-        for module in ("expat", "lcms2", "pystring", "yaml-cpp", "Imath"):
-            tools.remove_files_by_mask(os.path.join(self._source_subfolder, "share", "cmake", "modules"), "Find"+module+".cmake")
-
     def build(self):
-        self._patch_sources()
-
         cm = self._configure_cmake()
         cm.build()
 
@@ -125,20 +123,20 @@ class OpenColorIOConan(ConanFile):
         cm.install()
 
         if not self.options.shared:
-            self.copy("*", src=os.path.join(self.package_folder,
+            copy(self, pattern="*", src=os.path.join(self.package_folder,
                       "lib", "static"), dst="lib")
-            tools.rmdir(os.path.join(self.package_folder, "lib", "static"))
+            rmdir(self, os.path.join(self.package_folder, "lib", "static"))
 
-        tools.rmdir(os.path.join(self.package_folder, "cmake"))
-        tools.rmdir(os.path.join(self.package_folder, "lib", "pkgconfig"))
-        tools.rmdir(os.path.join(self.package_folder, "lib", "cmake"))
-        tools.rmdir(os.path.join(self.package_folder, "share"))
+        rmdir(self, os.path.join(self.package_folder, "cmake"))
+        rmdir(self, os.path.join(self.package_folder, "lib", "pkgconfig"))
+        rmdir(self, os.path.join(self.package_folder, "lib", "cmake"))
+        rmdir(self, os.path.join(self.package_folder, "share"))
         # nop for 2.x
-        tools.remove_files_by_mask(self.package_folder, "OpenColorIOConfig*.cmake")
+        rm(self, folder=self.package_folder, pattern="OpenColorIOConfig*.cmake", recursive=True)
 
-        tools.remove_files_by_mask(os.path.join(self.package_folder, "bin"), "*.pdb")
+        rm(self, folder=os.path.join(self.package_folder, "bin"), pattern="*.pdb", recursive=True)
 
-        self.copy("LICENSE", src=self._source_subfolder, dst="licenses")
+        copy(self, "LICENSE", src=self.source_folder, dst="licenses")
 
     def package_info(self):
         self.cpp_info.set_property("cmake_file_name", "OpenColorIO")
@@ -147,15 +145,16 @@ class OpenColorIOConan(ConanFile):
 
         self.cpp_info.libs = ["OpenColorIO"]
 
-        if tools.Version(self.version) < "2.1.0":
+        if Version(self.version) < "2.1.0":
             if not self.options.shared:
                 self.cpp_info.defines.append("OpenColorIO_STATIC")
 
         if self.settings.os == "Macos":
             self.cpp_info.frameworks.extend(["Foundation", "IOKit", "ColorSync", "CoreGraphics"])
 
-        if is_msvc(self) and not self.options.shared:
+        if self.settings.os == "Windows" and not self.options.shared:
             self.cpp_info.defines.append("OpenColorIO_SKIP_IMPORTS")
+            self.cpp_info.system_libs = ["gdi32"]
 
         bin_path = os.path.join(self.package_folder, "bin")
         self.output.info("Appending PATH env var with: {}".format(bin_path))
