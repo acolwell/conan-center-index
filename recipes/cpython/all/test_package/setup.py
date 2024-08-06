e5a78caca127a6a6dc792773b39411eca52bc751
Aaron Colwell <300262+acolwell@users.noreply.github.com>
2024-08-06 16:59:28 -0700

    cpython: Fix build-temp directory paths so they do not trigger Windows path limits.

--- recipes/cpython/all/test_package/setup.py
+++ recipes/cpython/all/test_package/setup.py
@@ -11,10 +11,18 @@ from setuptools import setup, Extension
 
 script_dir = os.path.dirname(os.path.realpath(__file__))
 
+# Make sure the script is run in the same directory as the source file.
+# Paths for various build outputs(.obj, .lib, etc.) are based on the source
+# file path. Running this script in the source directory ensures that these
+# are as short as possible.
+if not os.path.samefile(script_dir, os.getcwd()):
+    print("Setup must be run in the same directory as test_module.c")
+    exit(1)
+
 setup(
     name="test_package",
     version="1.0",
     ext_modules=[
-        Extension("spam", [os.path.join(script_dir, "test_module.c")]),
+        Extension("spam", ["test_module.c"]),
     ],
 )
