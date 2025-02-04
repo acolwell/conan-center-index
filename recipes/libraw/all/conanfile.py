4b345ad38da649ae23f15622e7acc9f714df14b1
Aaron Colwell <300262+acolwell@users.noreply.github.com>
2025-02-03 16:03:21 -0800

    Update libraw dependency on jasper to a new version that builds on Windows.

--- recipes/libraw/all/conanfile.py
+++ recipes/libraw/all/conanfile.py
@@ -64,7 +64,7 @@ class LibRawConan(ConanFile):
         if self.options.with_lcms:
             self.requires("lcms/2.16")
         if self.options.with_jasper:
-            self.requires("jasper/4.0.0")
+            self.requires("jasper/4.2.4")
 
     def validate(self):
         if self.settings.compiler.get_safe("cppstd"):
